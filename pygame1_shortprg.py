8a330f229ce9ab93042c210b79eb1eb7e9b177a2
ambikasrivastava582 <ambikasrivastava582@gmail.com>
2026-06-16 12:28:08 +0530

    Add files via upload

---
+++ pygame1_shortprg.py
@@ -0,0 +1,17 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((500, 400))
+pygame.display.set_caption("My First Pygame Window")
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.update()
+
+pygame.quit()
